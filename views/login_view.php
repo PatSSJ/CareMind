88a134e812f132dd8251880739124fa0cd5cfbb8
PatSSJ <patriciasoramasanjose@gmail.com>
2026-02-26 04:18:09 +0100

    Actualización de login_view.php

--- views/login_view.php
+++ views/login_view.php
@@ -1,10 +1,4 @@
-<?php 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-require_once("views/shared/header.php"); 
-?>
-
+<?php require_once("views/shared/header.php"); ?>
 
 <header>
 	<h1>CAREMIND</h1>
@@ -14,7 +8,7 @@ require_once("views/shared/header.php");
 <div class="contenedor">
 	<div class="columna izquierda">
     	<h2>Bienvenido de nuevo</h2>
-    	<p>Introduce tu identificación de cuidador para gestionar tus pacientes.</p>
+    	<p>Introduce tu identificación de cuidador para gestionar tus alarmas.</p>
     	<p>
             <a href="index.php?controller=auth&action=inicio" class="text-decoration-none">
                 Volver al inicio
@@ -24,12 +18,15 @@ require_once("views/shared/header.php");
 
 	<div class="columna derecha">
 
-        <?php if (isset($_SESSION['error'])): ?>
-            <div class="alert alert-danger">
-                <?php echo $_SESSION['error']; ?>
-            </div>
-            <?php unset($_SESSION['error']); ?>
-        <?php endif; ?>
+        <?php 
+	if (isset($_SESSION['error'])) {
+            echo "<div class='alert alert-danger'>";
+ 	    echo htmlspecialchars($_SESSION['error']);
+	    echo "</div>";
+
+            unset($_SESSION['error']);
+            }
+	 ?>
 
     	<form action="index.php?controller=auth&action=login" method="post">
 
